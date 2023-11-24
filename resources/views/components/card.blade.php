f77dd3c2312deadcfb5f6383d448b1dad94ad2c8
Michele Cascarano <miki300401@icloud.com>
2023-11-24 20:47:38 +0100

    style filterByCategory view

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -3,8 +3,9 @@
         <img src="{{Storage::url($product->img)}}" class="card-img-top" alt="...">
 
         <div class="card-body p-0">
+          <div>
           <h5 class="card-title mt-3 t-o fw-bolder">{{ $product->name }}</h5>
-          
+          </div>
           <p class="text-body" ><a class="text-decoration-none cardLink t-b fs-5" href="{{route('products.filterByCategory',['category'=>$product->category])}}">{{ $product->category->name }}</a></p>
           <p class="text-title fs-6">€{{ $product->price }}</p>
           <a href="{{ route('products.show', compact('product')) }}" class="btn bg-b rounded-5 t-n">Dettaglio</a>
--- resources/views/products/filterByCategory.blade.php
+++ resources/views/products/filterByCategory.blade.php
@@ -1,27 +1,15 @@
 <x-layout>
 
-<div class="container d-flex">
-    <div class="row justify-content-center align-items-center my-5">
-
-      <div class="col-12 py-5">
-        <h1>Categoria</h1>
+  <div class="board d-flex align-items-center">
+    <h1 class="ms-5 ps-5 board-text">Presto/{{ $category->name }}</h1>
+  </div>
+  <div class="container-fluid d-flex">
+  <div class="row justify-content-center align-items-center my-5 w-100">
+      <div class="col-12 py-4">
+        <h1 class="text-center">Esplora</h1>
       </div>
         @foreach ($category->products as $product)
-        <div class="col-12 col-md-6 my-3">
-        <div class="card justify-content-evenly">
-            <div class="card-img"></div>
-            <div class="card-info">
-              <p class="text-title">{{ $product->name }}</p>
-              <p class="text-body" ><a href="">{{ $product->category->name }}</a></p>
-            </div>
-            <div class="card-footer">
-            <span class="text-title">{{ $product->price }}</span>
-            <div class="card-button">
-              <a class="text-decoration-none" href="{{route('products.show', compact('product'))}}">dettaglio</a>
-            </div>
-          </div>
-        </div>
-    </div>
+        <x-card :product='$product' />
         
         @endforeach
     </div>
