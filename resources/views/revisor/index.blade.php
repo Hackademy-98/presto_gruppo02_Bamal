e8910b0f1213e709ea9e58f36f1a065525efe4e3
Letizia Cassano <letizia.cassano02@gmail.com>
2023-11-30 13:10:42 +0100

    modifiche css

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -29,13 +29,13 @@
                     @else
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                            <img src="/media/prova.jpg" class="d-block w-100" alt="...">
+                            <img src="/media/default.png" class="d-block w-100" alt="...">
                         </div>
                         <div class="carousel-item">
-                            <img src="/media/prova.jpg" class="d-block w-100" alt="...">
+                            <img src="/media/default.png" class="d-block w-100" alt="...">
                         </div>
                         <div class="carousel-item">
-                            <img src="/media/prova.jpg" class="d-block w-100" alt="...">
+                            <img src="/media/default.png" class="d-block w-100" alt="...">
                         </div>
                     </div>
                     @endif
