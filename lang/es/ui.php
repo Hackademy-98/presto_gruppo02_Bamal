66c6e17659e48e0254ff57dd4f58e325b7433333
Letizia Cassano <letizia.cassano02@gmail.com>
2023-11-30 18:15:38 +0100

    traduzione spagnolo

--- lang/es/ui.php
+++ lang/es/ui.php
@@ -1,4 +1,88 @@
 <?php
 return [
-    ''
+    'announcement'=>'Anuncios',
+    'workWithUs'=>'Trabaja con nosotros',
+    'revisorMenù'=>'Menú de revisor',
+    'newAnnouncement'=>'Nuevo anuncio',
+    'insertAnnouncement'=>'Insertar anuncio',
+    'login'=>'Acceso',
+    'register' => 'Iniciar sesión',
+    'logout'=>'Cerrar sesión',
+
+    'detailButton' => 'Detalle',
+
+    'latestAnnouncements'=> 'Ùltimos anuncios',
+    'allAnnouncements'=>'Todos los anuncios',
+    'search'=>'Cerca',
+    'categoryLabel'=>'Categoria',
+
+    'username'=>'Nombre de usuario',
+    'emailAddress'=> 'Correo electrónico',
+    'whyYouChooseUs'=>'¿Por qué nos elegiste?',
+    'sendApplication'=>'Enviar solicitud',
+
+    'revisorArea'=>'Àrea de revisor',
+    'hereAreTheAdsToReview'=>'Aquí están los anuncios para revisar',
+    'acceptButton'=>'Aceptar',
+    'refuseButton'=>'Rechazar',
+
+    'history'=>'Cronología',
+
+    'yourDashboard'=>'Tu tablero',
+    'yourAds'=>'Tus anuncios',
+    
+    //!Inserisci nuovo annuncio
+    'addNewAds'=>'Agregar un nuevo anuncio',
+    'name'=>'Nombre',
+    'description'=>'Descripción',
+
+    //!categorie
+    'categoryLabel'=>'Categoría',
+    'selectCategory'=>'Selecciona una categoría',
+    'Arredamento'=>'Muebles',
+    'Elettronica'=>'Electrónica',
+    'Veicoli'=>'Vehículos',
+    'Musica'=>'Música',
+    'Abbigliamento'=>'Ropa',
+    'Libri'=>'Libros',
+    'Videogiochi'=>'Juegos de vídeo',
+    'Animali'=>'Animales',
+    'Fai da te'=>'Bricolaje',
+    'Beauty'=>'Belleza',
+
+    //!Condizioni
+    'condionLabel'=>'Condición',
+    'selectCondition'=>'Seleccionar condición',
+    'Nuovo'=>'Nuevo',
+    'Come nuovo'=> 'Como nuevo',
+    'Ottimo'=>'Óptimo',
+    'Buono'=>'Bueno',
+    'Danneggiato'=>'Dañado',
+
+    'createAds'=>'Crear anuncios',
+
+    //!Login
+        'login' => 'Acceso',
+        'emailAddress'=> 'Correo electrónico',
+        'password'=>'Contraseña',
+        'submit'=>'Entregar',
+    
+        //!Register        
+        'passwordConfirmation'=>'Confirmar Contraseña',
+        'submit'=>'Entregar',
+
+    //!Card
+        'articleInfo'=>'Información del artículo',
+        //todo categorie eventuali
+        'createIn'=>'Crear el',
+        'createBy'=>'Creado por',
+        'mainData'=>'Informaciones principales',
+        'condition'=>'Condiciones',
+        //todo condizioni eventuali
+
+        'submitLogin'=>'Acceso',
+        'submitSignIn'=>'Entregar',
+
+        'thereAreNoAds'=>'No hay anuncios para esta búsqueda',
+        'thereAreNoAdsToReview'=>'No hay anuncios para revisar',
 ];
