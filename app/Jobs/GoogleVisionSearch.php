581c2ed3b04046cfaf69b85e0d1442e6c624d42c
Andrea Cibin <andreacibin@MBP-di-Andrea.Home>
2023-12-04 15:32:09 +0100

    avanz

---
+++ app/Jobs/GoogleVisionSearch.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Image;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Google\Cloud\Vision\V1\ImageAnnotatorClient;
+
+class GoogleVisionSearch implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    private $product_image_id;
+    public function __construct($product_image_id)
+    {
+        $this->product_image_id = $product_image_id;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $i = Image::find($this->product_image_id);
+        if (! $i) {
+    return;
+        }
+        $image = file_get_contents(storage_path("app/public/". $i->path));
+
+        putenv("GOOGLE_APPLICATION_CREDENTIALS =" . base_path("google_credetial.json"));
+
+        $imageAnnotator = new ImageAnnotatorClient();
+
+        $response = $imageAnnotator->safeSearchDetection($image);
+
+        $imageAnnotator-> close();
+
+        $safe = $response->getSafeSearchAnnotation();
+
+        $adult = $safe->getAdult();
+        $spoof = $safe->getSpoof();
+        $medical = $safe->getMedical();
+        $violence = $safe->getViolence();
+        $racy = $safe->getRacy();
+
+        $likeliHoodName = [
+            "text-secondary fas fa-circle", "text-success fas fa-circle", "text-success fas fa-circle", "text-warning fas fa-circle", "text-warning fas fa-circle", "text-danger fas fa-circle",
+        ];
+
+        $i->adult = $likeliHoodName[$adult];
+        $i->spoof = $likeliHoodName[$spoof];
+        $i->medical = $likeliHoodName[$medical];
+        $i->violence = $likeliHoodName[$violence];
+        $i->racy = $likeliHoodName[$racy];
+        $i->save();
+        
+    }
+}
